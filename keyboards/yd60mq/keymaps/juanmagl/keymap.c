5bb398ea16be50be3c3557ca3cb87e3863b621c4
juanma <juanmagl@gmail.com>
2022-01-15 13:06:07 +0100

    Cleaned FUNC and NUM layers. Left HOME<->END.

--- keyboards/yd60mq/keymaps/juanmagl/keymap.c
+++ keyboards/yd60mq/keymaps/juanmagl/keymap.c
@@ -44,25 +44,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,       KC_1,    KC_2,     KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,  KC_BSPC,
         KC_TAB,       KC_Q,    KC_W,     KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,
         TD(CAPS_CTL), KC_A,    KC_S,     KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_NUHS, KC_ENT,
-        KC_LSFT,      KC_NUBS, KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+        KC_LSFT,      KC_NUBS, KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,
         KC_LCTL,      KC_LGUI, KC_LALT,                              KC_SPC,                             KC_RALT, TD(FN_CTL), KC_APP,  KC_RCTL
 	),
 
     //,: Function Layer
     [_FUNC_LAYER] = LAYOUT_60_iso(
         KC_GRV,  KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, KC_HOME,  KC_UP,    KC_END,  KC_PGUP, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_SLCK, KC_PAUS,
-        _______, _______, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______,
-        _______, _______, _______,  _______,  KC_INS,  _______, _______, _______, _______, KC_INS,  _______, _______,          _______,
-        _______, KC_APP,  _______,                              _______,                            _______, _______, _______, _______
+        _______, XXXXXXX, KC_END,   KC_UP,    KC_HOME, KC_PGUP, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_SLCK, KC_PAUS,
+        _______, XXXXXXX, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX,  XXXXXXX,  KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX,          _______,
+        _______, _______, _______,                              _______,                            _______, _______, _______, _______
 	),
 
     //,: Numerical Layer
     [_NUM_LAYER] = LAYOUT_60_iso(
-        KC_GRV,  KC_A,    KC_B,     KC_C,     KC_D,    KC_E,    KC_F,    KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______, KC_NLCK, KC_BSPC,
-        _______, _______, KC_HOME,  KC_UP,    KC_END,  KC_PGUP, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______, _______,
-        _______, _______, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PEQL, _______, KC_PENT,
-        _______, _______, _______,  _______,  KC_INS,  _______, _______, _______, KC_P0,   KC_PCMM, KC_PDOT, KC_PPLS,          _______,
+        KC_GRV,  KC_A,    KC_B,     KC_C,     KC_D,    KC_E,    KC_F,    KC_P7,   KC_P8,   KC_P9,   KC_PSLS, XXXXXXX, KC_NLCK, KC_BSPC,
+        _______, XXXXXXX, KC_END,   KC_UP,    KC_HOME, KC_PGUP, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PEQL, XXXXXXX, KC_PENT,
+        _______, XXXXXXX, XXXXXXX,  XXXXXXX,  KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, KC_P0,   KC_PCMM, KC_PDOT, KC_PPLS,          _______,
         _______, _______, _______,                              _______,                            _______, _______, _______, _______
 	)
 
