7f2edff05e4adbb69ae0a3e43db59399dc063516
juanma <juanmagl@gmail.com>
2022-06-22 13:21:49 +0200

    Swap left Home and End in layer 1

--- keyboards/yd60mq/keymaps/juanmagl/keymap.c
+++ keyboards/yd60mq/keymaps/juanmagl/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,: Function Layer
     [_FUNC_LAYER] = LAYOUT_60_iso(
         KC_GRV,  KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, XXXXXXX, KC_END,   KC_UP,    KC_HOME, KC_PGUP, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_SLCK, KC_PAUS,
+        _______, XXXXXXX, KC_HOME,  KC_UP,    KC_END,  KC_PGUP, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_SLCK, KC_PAUS,
         _______, XXXXXXX, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_INS,  _______,
         _______, XXXXXXX, C(KC_Z),  C(KC_X),  C(KC_C), C(KC_V), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______,
         _______, _______, _______,                              _______,                            _______, _______, _______, _______
