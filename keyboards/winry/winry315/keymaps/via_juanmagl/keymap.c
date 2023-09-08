7741f8c7ff8179f2e65e7364618fad6282f72b31
juanma <juanmagl@gmail.com>
2023-09-08 11:36:20 +0200

    Initial version with 8 layers accessible by keys

--- keyboards/winry/winry315/keymaps/via_juanmagl/keymap.c
+++ keyboards/winry/winry315/keymaps/via_juanmagl/keymap.c
@@ -5,32 +5,58 @@
 
 // clang-format off
 
-#define U_LTESC LT(7, KC_ESC)
+#define U_LTESC LT(5, KC_ESC)
+#define U_LTTAB LT(6, KC_TAB)
+#define U_LTSPC LT(7, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_top(
             KC_HOME,           KC_MUTE,           KC_MPLY,
             TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
-             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-             U_LTESC, KC_TAB,  KC_SPC,  KC_BSPC, KC_ENT
+             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+             U_LTESC, U_LTTAB, U_LTSPC, KC_BSPC, KC_ENT
     ),
     [1] = LAYOUT_top(
             KC_HOME,           KC_MUTE,           KC_MPLY,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-             KC_TRNS, KC_TAB,  KC_SPC,  KC_BSPC, KC_ENT
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_ESC
     ),
-    [2 ... 6] = LAYOUT_top(
+    [2] = LAYOUT_top(
+            KC_TRNS,           KC_TRNS,           KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [3] = LAYOUT_top(
+            KC_TRNS,           KC_TRNS,           KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [4] = LAYOUT_top(
+            KC_TRNS,           KC_TRNS,           KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [5] = LAYOUT_top(
+            KC_TRNS,           KC_TRNS,           KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [6] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [7] = LAYOUT_top(
-            TO(5),             TO(6),             TO(7),
+            RGB_M_P,           RGB_M_B,           RGB_M_R,
              RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,
              RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,
-             KC_TRNS, RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R
+             KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_TOG
     ),
 };
 
@@ -39,9 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =        { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    [1] =        { ENCODER_CCW_CW(KC_1,    KC_3),    ENCODER_CCW_CW(KC_3,    KC_4),    ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    [2 ... 6] =  { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
-    [7] =        { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
+    [1] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [2] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [3] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [4] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [5] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [6] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [7] =        { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(RGB_SAD, RGB_SAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
 
