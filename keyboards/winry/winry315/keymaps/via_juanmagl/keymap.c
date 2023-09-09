bc1480c8638ea2d697e84244caf5b8eea14def4a
juanma <juanmagl@gmail.com>
2023-09-09 23:01:59 +0200

    Ten layers, switched by first row (1 or 2 presses)

--- keyboards/winry/winry315/keymaps/via_juanmagl/keymap.c
+++ keyboards/winry/winry315/keymaps/via_juanmagl/keymap.c
@@ -12,51 +12,63 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_top(
             KC_HOME,           KC_MUTE,           KC_MPLY,
-            TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
+            TO(5),    TO(1),   TO(2),   TO(3),   TO(4),
              KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-             U_LTESC, U_LTTAB, U_LTSPC, KC_BSPC, KC_ENT
+             KC_ESC,  KC_TAB,  KC_SPC,  KC_BSPC, KC_ENT
     ),
     [1] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            TO(0),    TO(6),   TO(2),   TO(3),   TO(4),
              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_ESC
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [2] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            TO(0),    TO(1),   TO(7),   TO(3),   TO(4),
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [3] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            TO(0),    TO(1),   TO(2),   TO(8),   TO(4),
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [4] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            TO(0),    TO(1),   TO(2),   TO(3),   TO(9),
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [5] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [6] = LAYOUT_top(
             KC_TRNS,           KC_TRNS,           KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [7] = LAYOUT_top(
+            KC_TRNS,           KC_TRNS,           KC_TRNS,
+            TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [8] = LAYOUT_top(
+            KC_TRNS,           KC_TRNS,           KC_TRNS,
+            TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [9] = LAYOUT_top(
             RGB_M_P,           RGB_M_B,           RGB_M_R,
-             RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,
-             RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,
-             KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_TOG
+            TO(0),    TO(1),   TO(2),   TO(3),   TO(4),
+             KC_NO,   KC_NO,   KC_NO,   RGB_MOD, RGB_SPI,
+             KC_NO,   KC_NO,   RGB_TOG, RGB_RMOD,RGB_SPD
     ),
 };
 
@@ -71,46 +83,67 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [4] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
     [5] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
     [6] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
-    [7] =        { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(RGB_SAD, RGB_SAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
+    [7] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [8] =        { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [9] =        { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(RGB_SAD, RGB_SAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t ledindex = 0;
+    uint8_t second_set = 0;
     switch(get_highest_layer(layer_state|default_layer_state)) {
+        case 9:
+            ledindex = 18; // Row 0, column 4
+            second_set = 1;
+            break;
+        case 8:
+            ledindex = 17; // Row 0, column 3
+            second_set = 1;
+            break;
         case 7:
-            ledindex = 1; // Right encoder, left led
+            ledindex = 12; // Row 0, column 2
+            second_set = 1;
             break;
         case 6:
-            ledindex = 3; // Center encoder, left led
+            ledindex = 11; // Row 0, column 1
+            second_set = 1;
             break;
         case 5:
-            ledindex = 5; // Left encoder, left led
+            ledindex = 6; // Row 0, column 0
+            second_set = 1;
             break;
         case 4:
             ledindex = 18; // Row 0, column 4
+            second_set = 0;
             break;
         case 3:
             ledindex = 17; // Row 0, column 3
+            second_set = 0;
             break;
         case 2:
             ledindex = 12; // Row 0, column 2
+            second_set = 0;
             break;
         case 1:
             ledindex = 11; // Row 0, column 1
+            second_set = 0;
             break;
         case 0:
             ledindex = 6; // Row 0, column 0
+            second_set = 0;
             break;
         default:
             ledindex = led_max;
     }
     if(ledindex < led_max) {
-        HSV hsv = {HSV_GREEN};
-        if(hsv.v > RGB_MATRIX_MAXIMUM_BRIGHTNESS) {
-            hsv.v = RGB_MATRIX_MAXIMUM_BRIGHTNESS;
+        HSV hsv_0 = {HSV_GREEN};
+        HSV hsv_1 = {HSV_YELLOW};
+        HSV *hsv = (second_set == 1 ? &hsv_1 : &hsv_0);
+        if(hsv->v > RGB_MATRIX_MAXIMUM_BRIGHTNESS) {
+            hsv->v = RGB_MATRIX_MAXIMUM_BRIGHTNESS;
         }
-        RGB rgb = hsv_to_rgb(hsv);
+        RGB rgb = hsv_to_rgb(*hsv);
         rgb_matrix_set_color(ledindex, rgb.r, rgb.g, rgb.b);
     }
     return false;
