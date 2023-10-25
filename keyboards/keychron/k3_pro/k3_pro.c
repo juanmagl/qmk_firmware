69e6b0eb7cb66a41ee756237f84046ced17d2eaa
juanma <juanmagl@gmail.com>
2023-10-26 00:51:56 +0200

    inverted OS layer assignment

--- keyboards/keychron/k3_pro/k3_pro.c
+++ keyboards/keychron/k3_pro/k3_pro.c
@@ -60,7 +60,11 @@ static void pairing_key_timer_cb(void *arg) {
 
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (index == 0) {
+#ifdef INVERT_OS_LAYER_ASSIGNMENT
+        default_layer_set(1UL << (active ? 2 : 0));
+#else
         default_layer_set(1UL << (active ? 0 : 2));
+#endif
     }
     dip_switch_update_user(index, active);
 
